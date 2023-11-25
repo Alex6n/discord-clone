4f1f4f8b425f02e5c9464db635d607ab072d622a
Alex6n <hheikal.work@gmail.com>
2023-11-25 15:19:23 +0200

    uploadThing setup

--- components/modals/Initial-modal.tsx
+++ components/modals/Initial-modal.tsx
@@ -2,11 +2,13 @@
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from 'react';
+
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input} from "@/components/ui/input";
 import { Button} from "@/components/ui/button";
-import { useEffect, useState } from 'react';
+import { FileUpload } from '../file-upload';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -36,7 +38,7 @@ export const InitialModal = () => {
     }
 
     if (!isMounted) return null;
-    
+
     return (
         <Dialog open>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -50,9 +52,23 @@ export const InitialModal = () => {
                 </DialogHeader>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <div className="space-y-8- px-6">
+                        <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint='serverImage'
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField
                                 control={form.control}
