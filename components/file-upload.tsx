1a22588c6714582ba9bbe5e0ba82e3d661a4f934
Alex6n <hheikal.work@gmail.com>
2023-11-26 16:00:26 +0200

    Server Creation API

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -17,7 +17,8 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
         return (
             <div className="relative h-20 w-20">
                 <Image fill src={value} alt="Upload" className="rounded-full"/>
-                <button className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                <button className="bg-rose-500 text-white p-1 rounded-full 
+                absolute top-0 right-0 shadow-sm"
                     onClick={() => onChange("")} type="button">
                     <X className="h-4 w-4" />
                 </button>
--- components/modals/Initial-modal.tsx
+++ components/modals/Initial-modal.tsx
@@ -1,4 +1,5 @@
 "use client"
+import axios from "axios";
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -9,6 +10,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input} from "@/components/ui/input";
 import { Button} from "@/components/ui/button";
 import { FileUpload } from '../file-upload';
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -21,6 +23,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
+    const router = useRouter();
     useEffect(() => { setIsMounted(true) }, []);
 
     const form = useForm({
@@ -34,7 +37,14 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values)
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if (!isMounted) return null;
