be58090cc45980d5339289efdcb4851be1338e38
Alex6n <hheikal.work@gmail.com>
2023-11-30 06:01:45 +0200

    Delete & Leave server modals

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,6 +70,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={()=> onOpen("deleteServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto"/>
@@ -77,6 +78,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={()=> onOpen("leaveServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/>
