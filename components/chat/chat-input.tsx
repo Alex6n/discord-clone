5ed9dfdfed6c353bc75753863e3b7e2c05406d07
Alex6n <hheikal.work@gmail.com>
2023-12-03 08:46:00 +0200

    Message API

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import * as z from 'zod';
+import axios from 'axios';
+import qs from 'query-string';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Plus, Smile } from 'lucide-react';
@@ -29,8 +31,17 @@ export const ChatInput = ({apiUrl, query, name, type} : ChatInputProps) => {
 
     const isLoading = form.formState.isSubmitting;
 
-    const onSubmit = async (value: z.infer<typeof formSchema>) => {
-        console.log(value);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const url = qs.stringifyUrl({
+                url: apiUrl,
+                query,
+            });
+
+            await axios.post(url, values);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
