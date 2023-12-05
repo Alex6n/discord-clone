ab525aba0fad57530c297b248fa6edaf7ce06df8
Alex6n <hheikal.work@gmail.com>
2023-12-05 13:03:05 +0200

    Video Calls

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack: (config) => {
-  //   config.externals.push({
-  //     "utf-8-validate": "commonjs utf-8-validate",
-  //     bufferutil: "commonjs bufferutil",
-  //   })
-  //   return config
+  // webpack: (
+  //   config,
+  //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  // ) => {
+  //   config.module.rules.push({
+  //     test: /\.mjs$/,
+  //     include: /node_modules/,
+  //     type: "javascript/auto",
+  //   });
+  //   return config;
   // },
+
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
   images: {
     domains: ["utfs.io"],
   },
